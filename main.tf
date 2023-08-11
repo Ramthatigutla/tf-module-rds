1066d42220d48ba907dcef815dc94eef9dcae651
ram <ramthatigutla@gmail.com>
2023-08-11 19:15:36 +0530

    ngw

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_rds_cluster" "main" {
   database_name          = var.db_name
   master_username        = data.aws_ssm_parameter.username.value
   master_password        = data.aws_ssm_parameter.password.value
-
-
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  kms_key_id = var.kms_key_id
 }
 
